d1f6729e6098f15dd06c0eebbac9ca19785faec4
LordGrim551 <lucajin551@gmail.com>
2024-02-12 12:14:06 +0100

    insertar
    insertar completado

--- TruequeWorld/app/src/main/java/com/example/truequeworld/Addproductscreen.java
+++ TruequeWorld/app/src/main/java/com/example/truequeworld/Addproductscreen.java
@@ -15,28 +15,6 @@ public class Addproductscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addproductscreen);
 
-        Spinner spinner = findViewById(R.id.Pricespinner);
-        final EditText editText = findViewById(R.id.Pricespinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.spinner_items, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                if (pos == 0) { // Si se selecciona el primer elemento del Spinner
-                    editText.setVisibility(View.GONE); // Oculta el EditText
-                } else {
-                    editText.setVisibility(View.VISIBLE); // Muestra el EditText
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // No es necesario implementar nada aquí
-            }
-        });
     }
 }
