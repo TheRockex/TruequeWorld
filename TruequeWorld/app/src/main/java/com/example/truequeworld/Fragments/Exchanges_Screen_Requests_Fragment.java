4a19351a78036ac59ad463ee1e412b888c89e90c
serijio <serijiomainsett@gmail.com>
2024-02-23 01:21:37 +0100

    cositas de layouts
    xd

---
+++ TruequeWorld/app/src/main/java/com/example/truequeworld/Fragments/Exchanges_Screen_Requests_Fragment.java
@@ -0,0 +1,44 @@
+package com.example.truequeworld.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.truequeworld.R;
+
+public class Exchanges_Screen_Requests_Fragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public Exchanges_Screen_Requests_Fragment() {}
+
+    public static Exchanges_Screen_Requests_Fragment newInstance(String param1, String param2) {
+        Exchanges_Screen_Requests_Fragment fragment = new Exchanges_Screen_Requests_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.f4_fragment_exchanges__screen__requests, container, false);
+        return view;
+    }
+}
